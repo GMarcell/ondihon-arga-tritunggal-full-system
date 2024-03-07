a8d08d07d4190cc6b8cef73727a3b6389ec9fe55
Grand Marcell <grand1310marcell@gmail.com>
2024-03-08 01:23:49 +0700

    admin layout done

---
+++ frontend/src/components/AdminNavbar.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import Dropdown from "../components/dropdown";
+import { FiAlignJustify } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import navbarimage from "../assets/img/layout/Navbar.png";
+import { BsArrowBarUp } from "react-icons/bs";
+import { FiSearch } from "react-icons/fi";
+import { RiMoonFill, RiSunFill } from "react-icons/ri";
+import {
+  IoMdNotificationsOutline,
+  IoMdInformationCircleOutline,
+} from "react-icons/io";
+import avatar from "../assets/img/avatars/avatar4.png";
+
+const AdminNavbar = (props) => {
+  const { onOpenSidenav, brandText } = props;
+
+  return (
+    <nav className="sticky top-4 z-40 flex flex-row flex-wrap items-center justify-between rounded-xl bg-white p-2 backdrop-blur-xl">
+      <span
+        className="flex cursor-pointer text-3xl text-gray-600 xl:hidden"
+        onClick={onOpenSidenav}
+      >
+        <FiAlignJustify className="h-8 w-8" />
+      </span>
+      <div className="ml-[6px]">
+        <p className="shrink text-[33px] font-bold capitalize text-black">
+          {brandText}
+        </p>
+      </div>
+
+      <div className="relative mt-[3px] flex h-[61px] flex-grow items-center justify-around gap-2 rounded-full px-2 py-2 shadow-shadow-500 md:flex-grow-0 md:gap-1 xl:gap-2">
+        {/* Profile & Dropdown */}
+        <Dropdown
+          classNames={"py-2 top-8 -left-[180px] w-max"}
+          button={
+            <img
+              className="h-12 w-12 rounded-full"
+              src={avatar}
+              alt="Elon Musk"
+            />
+          }
+          children={
+            <div className="flex w-56 flex-col justify-start rounded-[20px] bg-white bg-cover bg-no-repeat shadow-xl shadow-shadow-500">
+              <div className="flex flex-col p-4">
+                <a
+                  href=" "
+                  className="mt-3 text-md font-medium text-red-500 hover:text-red-500 transition duration-150 ease-out hover:ease-in"
+                >
+                  Log Out
+                </a>
+              </div>
+            </div>
+          }
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default AdminNavbar;
--- frontend/src/layout/AdminLayout.jsx
+++ frontend/src/layout/AdminLayout.jsx
@@ -1,21 +1,51 @@
 import React, { useState } from "react";
 import { useStateContext } from "../hooks/stateContext";
-import { Navigate } from "react-router-dom";
-import Sidebar from '../components/sidebar'
+import { Navigate, useLocation } from "react-router-dom";
+import Sidebar from "../components/sidebar";
+import AdminNavbar from "../components/AdminNavbar";
 
 function AdminLayout() {
   const { user, token } = useStateContext();
-  const [open, setOpen] = useState(true)
+  const [open, setOpen] = useState(true);
 
   if (!token) {
     return <Navigate to="/" />;
   }
 
+  const currentPath = useLocation().pathname.split('/').pop()
+
   return (
-  <>
-    <Sidebar open={open} onClose={() => setOpen(false)} />
-    hesa
-  </>
+    <div className="flex h-full w-full">
+      <Sidebar open={open} onClose={() => setOpen(false)} />
+      <div className="h-full w-full bg-lightPrimary dark:!bg-navy-900">
+        {/* Main Content */}
+        <main
+          className={`mx-[12px] h-full flex-none transition-all md:pr-2 xl:ml-[313px]`}
+        >
+          {/* Routes */}
+          <div className="h-full">
+            <AdminNavbar
+              onOpenSidenav={() => setOpen(true)}
+              logoText={"Horizon UI Tailwind React"}
+              brandText={currentPath}
+            />
+            {/* <div className="pt-5s mx-auto mb-auto h-full min-h-[84vh] p-2 md:pr-2">
+              <Routes>
+                {getRoutes(routes)}
+
+                <Route
+                  path="/"
+                  element={<Navigate to="/admin/default" replace />}
+                />
+              </Routes>
+            </div>
+            <div className="p-3">
+              <Footer />
+            </div> */}
+          </div>
+        </main>
+      </div>
+    </div>
   );
 }
 
