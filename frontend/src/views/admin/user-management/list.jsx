f6bc7e654be32f2fcd9587beab0443dff9774597
Grand Marcell <grand1310marcell@gmail.com>
2024-03-11 23:49:09 +0700

    backend for crud user done

--- frontend/src/views/admin/user-management/list.jsx
+++ frontend/src/views/admin/user-management/list.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axiosClient from '../../../axios-client'
 
 function UserManagementList() {
+  const [isLoading, setisLoading] = useState([])
+
+  const getUsers = () => {
+    setisLoading(true)
+    axiosClient.get('/users').then(({data}) => {
+      setisLoading(false)
+      console.log(data)
+    }).catch(() => {
+      setisLoading(false)
+    })
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+
   return (
     <div>UserManagementList</div>
   )
