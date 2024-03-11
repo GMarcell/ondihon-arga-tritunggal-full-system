b184f37724b5ce0b5789faf63c60c08b85f76e84
Grand Marcell <grand1310marcell@gmail.com>
2024-03-11 19:36:41 +0700

    change sidebar icon

--- frontend/src/utils/adminRoute.jsx
+++ frontend/src/utils/adminRoute.jsx
@@ -1,4 +1,7 @@
-import { MdHome } from 'react-icons/md'
+import { MdHome, MdSupervisedUserCircle } from 'react-icons/md'
+import { PiUsersThreeFill } from "react-icons/pi";
+import { GiNewspaper } from "react-icons/gi";
+import { TbArticleFilledFilled } from "react-icons/tb";
 
 const adminRoute = [
   {
@@ -9,22 +12,22 @@ const adminRoute = [
   {
     name: 'User Management',
     path: '/administrator/user-management',
-    icon: <MdHome className="h-6 w-6" />,
+    icon: <PiUsersThreeFill className="h-6 w-6" />,
   },
   {
     name: 'Customer Management',
     path: '/administrator/customer-management',
-    icon: <MdHome className="h-6 w-6" />,
+    icon: <MdSupervisedUserCircle className="h-6 w-6" />,
   },
   {
     name: 'News Management',
     path: '/administrator/news-management',
-    icon: <MdHome className="h-6 w-6" />,
+    icon: <GiNewspaper className="h-6 w-6" />,
   },
   {
     name: 'Article Management',
     path: '/administrator/article-management',
-    icon: <MdHome className="h-6 w-6" />,
+    icon: <TbArticleFilledFilled className="h-6 w-6" />,
   },
 ]
 
