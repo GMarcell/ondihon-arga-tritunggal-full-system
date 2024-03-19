90aa0394ea8f131a5102707d87034c4f50062649
Grand Marcell <grand1310marcell@gmail.com>
2024-03-19 15:51:16 +0700

    fix styling for news image and youtube iframe

--- frontend/src/views/admin/news-management/preview.jsx
+++ frontend/src/views/admin/news-management/preview.jsx
@@ -1,28 +1,36 @@
 import React from "react";
 
 function NewsPreview(props) {
-  const youtubeID = props?.video?.split('=')?.pop()
+  const youtubeID = props?.video?.split("=")?.pop();
 
   return (
     <div className="w-full">
-      <h3 className="font-bold text-brand-500 text-center text-md capitalize text-[33px]">{props?.title ?? "-"}</h3>
-      <div className="p-3">
-      <img src={props?.isOldData ? import.meta.env.VITE_API_BASE_URL + "/storage/" + props?.image : props?.image} />
+      <h3 className="font-bold text-brand-500 text-center text-md capitalize text-[33px]">
+        {props?.title ?? "-"}
+      </h3>
+      <div className="p-3 flex justify-center items-center">
+        <img
+          src={
+            props?.isOldData
+              ? import.meta.env.VITE_API_BASE_URL + "/storage/" + props?.image
+              : props?.image
+          }
+          className="max-h-96"
+        />
       </div>
       <h3>{props?.desc ?? "-"}</h3>
-      {
-        props?.video && (
-          <div className="flex justify-center items-center mt-3">
-            <iframe
-              src={`https://www.youtube.com/embed/${youtubeID}`}
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-              title="Embedded youtube"
-            />
-          </div>
-        )
-      }
+      {props?.video && (
+        <div className="flex justify-center items-center mt-3">
+          <iframe
+            src={`https://www.youtube.com/embed/${youtubeID}`}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title="Embedded youtube"
+            className="w-2/3 max-w-xl"
+          />
+        </div>
+      )}
     </div>
   );
 }
